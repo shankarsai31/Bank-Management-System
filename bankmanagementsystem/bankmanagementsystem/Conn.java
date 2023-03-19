e3186572552f1a1654c08861af85cbea80a81ed6
shankarsai31 <107424540+shankarsai31@users.noreply.github.com>
2023-03-19 12:32:35 +0530

    Add files via upload

---
+++ bankmanagementsystem/bankmanagementsystem/Conn.java
@@ -0,0 +1,23 @@
+
+package bankmanagementsystem;
+
+import java.sql.*;
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+            c=DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","saishankar@12");
+            s=c.createStatement();
+            
+            
+        
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+   
+   
+}
